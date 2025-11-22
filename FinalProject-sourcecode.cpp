4e603f97b4149bd3c7e05b27782fce2a1a0d9195
Lizzy-143 <vasquezlizzy737@gmail.com>
2025-11-21 19:35:51 -0600

    Update FinalProject-sourcecode.cpp

--- FinalProject-sourcecode.cpp
+++ FinalProject-sourcecode.cpp
@@ -3,7 +3,7 @@
 #include <cstring>
 using namespace std;
 
-class Insurance{
+class Insurance{//Base Class
   protected:
     int PolicyNumber;
     char customername [15];
@@ -12,7 +12,15 @@ class Insurance{
     string expiryDate;
     char status[15];
 public:
-    Insurance(int pn, char* cname, int pv, float mp, string ed, char* s){
+    Insurance(){//default Insurance constructor
+      PolicyNumber=0;
+      strcpy(customername, "Unknown");
+      policyvalue = 0;
+      monthlyPayment = 0.0;
+      expiryDate = "N/A";
+      strcpy(status, "Active");
+    }
+    Insurance(int pn, char* cname, int pv, float mp, string ed, char* s){//Insurance constructor with parameters
         PolicyNumber = pn;
         strcpy(customername, cname);
         policyvalue = pv;
@@ -24,7 +32,6 @@ virtual void displayPolicyInfo(){
     cout << "~~~~ Policy Info ~~~~\n";
     cout << "Number: "<< PolicyNumber << " | Name: " << customername << "\n"; 
 }
-    
-    
-    
+virtual double calculatePremium() = 0; //pure virtual that forces overrride in subclasses 
 };
+
